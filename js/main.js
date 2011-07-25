f362b118e9a777cb5213fafc226f6adfb7b6742c
sprjr <me@stephenrivasjr.com>
2011-07-25 20:18:14 +0000

    Updated Meta Info functinoality

--- js/main.js
+++ js/main.js
@@ -20,9 +20,21 @@ var loadSlide = function() {
         slideShow = $('#slideshow');
         
     slideShow.animate({opacity: 0}, function(){
+        links.empty();
         body.html('<p>' + currentSlide.body + '</p>');
         caption.html('&quot;' + currentSlide.caption + '&quot;');
         title.text(currentSlide.title);
+        metaList.find('li:first-child').text('by ' + currentSlide.author);
+        metaList.find('li:nth-child(2)').text('category: ' + currentSlide.category);
+        
+        if(currentSlide.links) {
+            links.append('<li>Links to check out:</li>');
+            for (var i=0, j=currentSlide.links.length; i<j; i++) {
+                var link = currentSlide.links[i];
+                links.append('<li><a href="' + link + '">' + link + '</a></li>');
+            }
+        }
+        
         slideShow.animate({opacity: 1});
     });
 }
